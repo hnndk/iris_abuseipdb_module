fcffa9355d65364186b76b7e6321446c1080048e
hndk <hndk8080@gmail.com>
2026-07-17 13:26:35 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# iris_abuseipdb_module
+
+iris_abuseipdb_module` is a IRIS pipeline/processor module created with https://github.com/dfir-iris/iris-skeleton-module
+
+Copyright 2026, iris under the License MIT
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='iris_abuseipdb_module',
+    python_requires='>=3.9',
+    version='0.1.0',
+    packages=['iris_abuseipdb_module', 'iris_abuseipdb_module.abuseipdb_handler'],
+    url='https://github.com/hnndk/iris_abuseipdb_module',
+    license='MIT',
+    author='iris',
+    author_email='andre.c.carvalhais@aubay.pt',
+    description='iris_abuseipdb_module` is a IRIS pipeline/processor module created with https://github.com/dfir-iris/iris-skeleton-module',
+    install_requires=[]
+)
