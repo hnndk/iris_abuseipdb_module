23c4b9588709722dca3cf58b80d74c3de0e28143
hndk <hndk8080@gmail.com>
2026-07-22 16:57:43 +0100

    Bump version from 1.1 to 1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='iris-abuseipdb-module',
     python_requires='>=3.9',
-    version='1.1',
+    version='1.2',
     packages=['iris_abuseipdb_module', 'iris_abuseipdb_module.abuseipdb_handler'],
     url='https://github.com/cyber/iris-abuseipdb-module',
     license='MIT',
